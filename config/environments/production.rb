8d3ac07ea03cf1e5e25e0fd84f70e7bb48eceae0
dicotha <wbn_k@hotmail.com>
2017-06-27 18:47:48 +0000

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,11 @@ Rails.application.configure do
       url: ':s3_domain_url',
       path: ':class/:attachment/:id/:style.:extension',
       s3_region: ENV.fetch('AWS_REGION'),
-      s3_host_name: 's3.east-us-2.amazonaws.com',
       s3_credentials: {
           bucket: ENV.fetch('S3_BUCKET_NAME'),
           access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
           secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+          s3_host_name: ENV.fetch('S3_ENDPOINT'),
       }
   }
 end
