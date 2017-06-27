c4161e5cf617e5dc9cae5f22442235e3d2d87e77
dicotha <wbn_k@hotmail.com>
2017-06-27 18:54:06 +0000

    retorn to the old code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,13 @@ Rails.application.configure do
   #added as per devise installation instructions 
   config.action_mailer.default_url_options = { host: "courseudemyruby.herokuapp.com" }
  
-   config.paperclip_defaults = {
-      storage: :s3,
-      url: ':s3_domain_url',
-      path: ':class/:attachment/:id/:style.:extension',
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-      s3_credentials: {
-          bucket: ENV.fetch('S3_BUCKET_NAME'),
-          access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-          secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-          s3_host_name: ENV.fetch('S3_ENDPOINT'),
-      }
+    }
   }
 end
