d70e5d71e4b180c4540aa84363a99d59bd16bb23
dicotha <wbn_k@hotmail.com>
2017-06-27 18:44:54 +0000

    add new conf to production to run paperclip in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,16 @@ Rails.application.configure do
   #added as per devise installation instructions 
   config.action_mailer.default_url_options = { host: "courseudemyruby.herokuapp.com" }
  
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+   config.paperclip_defaults = {
+      storage: :s3,
+      url: ':s3_domain_url',
+      path: ':class/:attachment/:id/:style.:extension',
+      s3_region: ENV.fetch('AWS_REGION'),
+      s3_host_name: 's3.east-us-2.amazonaws.com',
+      s3_credentials: {
+          bucket: ENV.fetch('S3_BUCKET_NAME'),
+          access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+          secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      }
   }
-}
 end
